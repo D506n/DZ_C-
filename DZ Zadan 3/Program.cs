bc4d0a829d6d0172a76dcb06b26f2aac1d5f996e
Dmitriy Medvedev <D506n@yandex.ru>
2023-04-24 18:37:16 +0300

    Написал программу для проверки числа на чётность

---
+++ DZ Zadan 3/Program.cs
@@ -0,0 +1,10 @@
+﻿Console.WriteLine("Введите число:");
+int NumA = Convert.ToInt32(Console.ReadLine());
+if (NumA % 2 == 0){
+    Console.WriteLine($"Число {NumA} - чётное");
+}
+else{
+    Console.WriteLine($"Число {NumA} - нечётное");
+}
+Console.WriteLine("Введите любой символ для выхода"); //Добавил для того, чтобы окно консоли не закрывалось сразу как только выдаст результат
+Console.ReadLine();
