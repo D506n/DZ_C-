94eb9cf671bba81377b9973cdd9b6a65e4d57332
Dmitriy Medvedev <D506n@yandex.ru>
2023-04-24 18:26:52 +0300

    Написал программу для вывода максимального из трёх чисел

---
+++ DZ Zadan 2/Program.cs
@@ -0,0 +1,18 @@
+﻿Console.WriteLine("Введите первое число:");
+int NumA = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите второе число:");
+int NumB = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите третье число:");
+int NumС = Convert.ToInt32(Console.ReadLine());
+int Max = NumA;
+if (Max < NumB)
+{
+    Max = NumB;
+}
+if (Max < NumС)
+{
+    Max = NumС;
+}
+Console.WriteLine($"Максимальное число: {Max}");
+Console.WriteLine("Введите любой символ для выхода"); //Добавил для того, чтобы окно консоли не закрывалось сразу как только выдаст результат
+Console.ReadLine();
