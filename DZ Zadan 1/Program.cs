569e0d71d0d4265ef1c06da8b506c9e0b9fd3500
Dmitriy Medvedev <D506n@yandex.ru>
2023-04-24 18:11:34 +0300

    Написал программу для сравнения двух чисел

---
+++ DZ Zadan 1/Program.cs
@@ -0,0 +1,14 @@
+﻿Console.WriteLine("Введите первое число:");
+int NumA = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите второе число:");
+int NumB = Convert.ToInt32(Console.ReadLine());
+if (NumA > NumB)
+{
+    Console.WriteLine($"{NumA}>{NumB}");
+}
+else
+{
+    Console.WriteLine($"{NumB}>{NumA}");
+}
+Console.WriteLine("Введите любой символ для выхода"); //Добавил для того, чтобы окно консоли не закрывалось сразу как только выдаст результат
+Console.ReadLine();
