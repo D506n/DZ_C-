436e634d9d228e1c80674097f55597af7e526675
Dmitriy Medvedev <D506n@yandex.ru>
2023-04-25 11:29:20 +0300

    Написал программу для вывода чётных чисел от 1 до N

---
+++ DZ Zadan 4/Program.cs
@@ -0,0 +1,23 @@
+﻿Console.WriteLine("Введите число:");
+int NumA = Convert.ToInt32(Console.ReadLine());
+
+//Создал массив
+int[] array = new int[NumA];
+
+//Теперь заполняю массив числами от 1 до введённого пользователем
+int Step = 0; 
+array[Step] = 1;
+while(Step< NumA - 1) {
+     Step = Step +1;
+    array[Step] = array[Step] + Step + 1;
+}
+
+//вывожу чётные числа от 1 до N
+Console.Write("Чётные числа: ");
+Step = 1;
+while(Step < NumA){
+    if(array[Step] %2==0){
+        Console.Write($"{array[Step]} ");
+    }
+    Step = Step +1;
+}
